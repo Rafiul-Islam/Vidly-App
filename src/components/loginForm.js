a00f7eca327a7d6ffee58aa1fb5e660c6c5e7ce4
Rafiul-Islam <rafi.cse.ru@gmail.com>
2023-02-05 12:36:27 +0600

    login route adde

--- src/components/loginForm.js
+++ src/components/loginForm.js
@@ -3,7 +3,25 @@ import React from 'react';
 const LoginForm = () => {
     return (
         <div>
-            <h1>Login</h1>
+            <h1 className='mb-4'>Login</h1>
+            <form action="">
+                <div className="mb-3">
+                    <label htmlFor="formGroupExampleInput" className="form-label">Username</label>
+                    <input type="text"
+                           className="form-control"
+                           id="username"
+                    />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="formGroupExampleInput2" className="form-label">Password</label>
+                    <input
+                        type="password"
+                        className="form-control"
+                        id="password"
+                    />
+                </div>
+                <button className='btn btn-primary'>Login</button>
+            </form>
         </div>
     );
 };
